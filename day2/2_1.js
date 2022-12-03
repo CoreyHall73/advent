4146ceb0c704de11446e31add8c0140c93342c23
Corey Hall <coreyhall73@yahoo.com>
2022-12-03 15:45:26 -0500

    Nearly done with 2_1

--- day2/2_1.js
+++ day2/2_1.js
@@ -1,22 +1,62 @@
-const {readFileSync, promises: fsPromises} = require('fs');
+const { readFileSync, promises: fsPromises } = require('fs');
 
 function winStrat() {
 
-    const contents = readFileSync("2_1.txt", 'utf-8');
+	const contents = readFileSync("2_1.txt", 'utf-8');
 	const strArr = contents.split(/\r?\n/);
-    //console.log(strArr);
-    
-    for (i = 0; i < strArr.length; i++) {
-		//console.log("pairs: ", strArr[i]);
-		for (j = 0; j < strArr[i].length; j++) {
-			//console.log();
-			if (strArr[i][0] == "A" && strArr[i][2] == "Y") {
-				console.log("Case 1");
-			} else {
-				//console.log("others");
+	let score = 0;
+	const win = 6;
+	const draw = 3;
+	const rock = 1;
+	const paper = 2;
+	const scissors = 3; 
+
+	for (i = 0; i < strArr.length; i++) {
+		
+		if (strArr[i][0] == "A") {
+			if (strArr[i][2] == "X") {
+				score += (rock + draw);
+				console.log("draw: ", rock, draw);
+			}
+			if (strArr[i][2] == "Y") {
+				score += rock;
+				console.log("loss : ", rock);
+			}
+			if (strArr[i][2] == "Z") {
+				score += (rock + win);
+				console.log("win: ", rock, win);
 			}
 		}
+		if (strArr[i][0] == "B") {
+			if (strArr[i][2] == "X") {
+				score += (paper + win);
+				console.log("win: ", paper, win);
+			}
+			if (strArr[i][2] == "Y") {
+				score += (paper + draw);
+				console.log("draw : ", paper, draw);
+			}
+			if (strArr[i][2] == "Z") {
+				score += paper;
+				console.log("loss: ", paper);
+			}		
+		}
+		if (strArr[i][0] == "C") {
+			if (strArr[i][2] == "X") {
+				score += scissors;
+				console.log("lose: ", scissors);
+			}
+			if (strArr[i][2] == "Y") {
+				score += (scissors + win);
+				console.log("win : ", scissors, win);
+			}
+			if (strArr[i][2] == "Z") {
+				score += (scissors + draw);
+				console.log("draw: ", draw);
+			}		
+		}
 	}
+	console.log(score);
 }
 
 winStrat();
