e362e55284b43c957a04b72b3979de6d72a7dc90
Corey Hall <coreyhall73@yahoo.com>
2022-12-04 16:51:08 -0500

    Trying to parse data

--- day4/4_1.js
+++ day4/4_1.js
@@ -6,17 +6,27 @@ function overlap() {
 	const contents = readFileSync("4.txt", 'utf-8');
 	const strArr = contents.split(/\r?\n/);
 	let intArr = [];
+	let testA = [];
 
 	//console.log(strArr);
 
 	for (i = 0; i < strArr.length; i++) {
 		//console.log(strArr[i]);
 		//console.log(strArr[i+1]);
-		intArr.push([parseInt([strArr[0]])], [parseInt([strArr[1]])]);
+		for (j = 0; j < strArr[i].length; j++) {
+			intArr.push(parseInt(strArr[i][j]));
+			if (typeof intArr[j] == "number") {
+				testA.push(parseInt(intArr[j]));
+			} else if (typeof intArr[j] == "NaN") {
+				testA.push("hit");
+			}
+		}
+		
 		break;
 	}
 		//console.log(strArr);
 		console.log(intArr);
+		console.log(testA);
 
 }
 
