569722e11da040de5b7bea7fc861b1afafb79b27
Hall <625546@bah.com>
2022-12-06 15:30:30 -0500

    part 1 solved

--- day6/6_1.js
+++ day6/6_1.js
@@ -5,20 +5,32 @@ const lines = readFileSync("6.txt", { encoding: "utf-8" }) // read day??.txt con
 	.trim() // Remove starting/ending whitespace
 	//.split("\n") // Split on newline
 
+
 function receive() {
 	let afterIndex = 0;
-	//console.log(lines);
-	for (i = 0; i < lines.length; i++) {
-		afterIndex += 4;
-		//console.log("value:", lines[i], "at index:", i);
-		//console.log(lines[i], lines[i + 1], lines[i + 2], lines[i + 3], afterIndex);
+	console.log(lines);
+	for (i = 0; i < lines.length - 3; i++) {
+		
+		afterIndex++;
+		
 		let group = lines[i] + lines[i + 1] + lines[i + 2] + lines[i + 3];
-		if (group != null) {
-			console.log(group);
+		//console.log(group ,i);
+		for (j = 0; j < group.length; j++) {
+			//console.log(group[j]);
+			if ((group[j] != group[j+1]) && (group[j] != group[j+2]) && (group[j] != group[j+3]) && (group[j+1] != group[j+2])
+			&& (group[j+1] != group[j+3]) && (group[j+2] != group[j+3]) && (group[j+3] != undefined)) {
+				console.log("group:", group[j], group[j+1], group[j+2], group[j+3], afterIndex);
+				break;
+			}
+			break;
 		}
 		// Edge case for last 3 groups
+		if (group.length > 4) {
+			//console.log("incomplete group");
+			break;
+		}
+	console.log(afterIndex + 3);
 	}
-	//console.log(group);
 }
 
 receive();
